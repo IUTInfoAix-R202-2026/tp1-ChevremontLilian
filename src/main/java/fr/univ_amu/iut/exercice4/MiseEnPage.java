49397594f676ad51a60aae3e11d0ac2acceb4181
Lilian Chevremont <lilian.chevremont@etu.univ-amu.fr>
2026-04-29 14:13:14 +0000

    Exercice 4 terminé

--- src/main/java/fr/univ_amu/iut/exercice4/MiseEnPage.java
+++ src/main/java/fr/univ_amu/iut/exercice4/MiseEnPage.java
@@ -1,6 +1,15 @@
 package fr.univ_amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -34,16 +43,35 @@ public class MiseEnPage extends Application {
     // Stratégie conseillée :
     //
     // 1. Un BorderPane comme racine (setTop, setCenter, setBottom).
-    // 2. Top    : un MenuBar avec deux Menu "Fichier" et "Aide".
+    // 2. Top : un MenuBar avec deux Menu "Fichier" et "Aide".
     // 3. Center : un GridPane avec 2 lignes / 2 colonnes :
-    //                (0,0) Label "Nom :"     | (1,0) TextField
-    //                (0,1) Label "Email :"   | (1,1) TextField
+    // (0,0) Label "Nom :" | (1,0) TextField
+    // (0,1) Label "Email :" | (1,1) TextField
     // 4. Bottom : un HBox contenant deux boutons "Valider" et "Annuler",
-    //             avec un peu d'espacement et un padding.
+    // avec un peu d'espacement et un padding.
     //
     // Donne un id CSS utile sur les composants si tu veux les retrouver
     // facilement (les tests utilisent lookup sur les classes ".text-field"
     // et ".button", pas besoin d'id particulier).
+    BorderPane monBorderPane = new BorderPane();
+    MenuBar monMenu = new MenuBar();
+    Menu file = new Menu("Fichier");
+    Menu help = new Menu("Aide");
+    monMenu.getMenus().add(file);
+    monMenu.getMenus().add(help);
+    monBorderPane.setTop(monMenu);
+    GridPane grid = new GridPane();
+    grid.add(new Label("Nom :"), 0, 0);
+    grid.add(new TextField(), 1, 0);
+    grid.add(new Label("Email :"), 0, 1);
+    grid.add(new TextField(), 1, 1);
+    monBorderPane.setCenter(grid);
+    HBox box = new HBox(8);
+    box.getChildren().addAll(new Button("Valider"), new Button("Annuler"));
+    monBorderPane.setBottom(box);
+    Scene maScene = new Scene(monBorderPane);
+    primaryStage.setScene(maScene);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
